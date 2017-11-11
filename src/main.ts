34fdf431577355d089a5803f7fd00ad0505e420f
Bryan Phelps <Bryan Phelps>
2017-11-10 16:53:07 -0800

    Incorporate write-good diagnostics for the markdown language server

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,29 @@ connection.onInitialize((params: InitializeParams): InitializeResult => {
 
 const documentContent: { [documentUri: string]: string } = {}
 
+const writeGood = require("write-good")
+
+interface IWriteGoodSuggestion {
+    reason: string
+    index: number // line
+    offset: number // character
+}
+
+documents.onDidChangeContent(change => {
+    const suggestions: IWriteGoodSuggestion[] = writeGood(change.document.getText())
+
+    const diagnostics = suggestions.map((suggestion:  IWriteGoodSuggestion) => {
+        const position = change.document.positionAt(suggestion.index)
+        const diagnostic: types.Diagnostic = {
+            message: suggestion.reason,
+            range: types.Range.create(position.line, position.character, position.line, position.character + suggestion.offset),
+        }
+
+        return diagnostic
+    })
+
+    connection.sendDiagnostics({ uri: change.document.uri, diagnostics })
+})
 
 connection.onHover(async (textDocumentPosition): Promise<types.Hover> => {
 
